b8846ef7a61463636a511662bb99601835e5b9c2
Zuchi-nest <74808623+Zuchi-nest@users.noreply.github.com>
2021-03-02 12:32:39 +0800

    Update BlogDetails.js

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,28 +1,25 @@
-import { useHistory, useParams } from "react-router-dom";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
-  const { data: blog, error, isPending } = useFetch(process.env.REACT_APP_API + "/" + id);
-  const history = useHistory();
+  const { data, error, isPending } = useFetch("http://localhost:3001/blogs/" + id);
+
 
   const handleClick = () => {
-    fetch('http://localhost:3001/blogs/' + blog.id, {
-      method: 'DELETE'
-    }).then(() => {
-      history.push('/');
-    }) 
+    axios.delete("http://localhost:3001/delete/" + id)
   }
 
   return (
     <div className="blog-details">
       { isPending && <div>Loading...</div> }
       { error && <div>{ error }</div> }
-      { blog && (
+      { data && (
         <article>
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
-          <div>{ blog.body }</div>
+          <h2>{ data.title }</h2>
+          <p>Written by { data.author }</p>
+          <div>{ data.body }</div>
           <button onClick={handleClick}>delete</button>
         </article>
       )}
